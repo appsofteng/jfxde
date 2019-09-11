08d690a2965d40c39b466820a1384260a9aa15c8
bezvaapp <19367016+bezvaapp@users.noreply.github.com>
2019-09-11 19:10:43 +0200

    Fixed the log path.

--- dev.jfxde.parent/dev.jfxde.logic/src/main/java/dev/jfxde/logic/FileManager.java
+++ dev.jfxde.parent/dev.jfxde.logic/src/main/java/dev/jfxde/logic/FileManager.java
@@ -10,25 +10,26 @@ import  dev.jfxde.logic.Constants;
 
 public final class FileManager extends Manager {
 
-    private static final String HOME_DIR = System.getProperty("user.home") + "/." + Constants.SYS_NAME + "/" + Constants.SYS_VERSION + "/" + "users/default/";
-    private static final String USER_DIR = System.getProperty("user.dir") + "/";
+    private static final String HOME_DIR = System.getProperty("user.home") + "/." + Constants.SYS_NAME + "/" + Constants.SYS_VERSION + "/";
+    private static final String USER_DIR = HOME_DIR + "users/default/";
+    private static final String START_DIR = System.getProperty("user.dir") + "/";
 
-    public static final Path HOME_APPS_DIR = Paths.get(HOME_DIR + "apps");
-    public static final Path DATA_DIR = Paths.get(HOME_DIR + "data");
-    public static final Path CONF_DIR = Paths.get(HOME_DIR + "conf");
-    public static final Path DB_DIR = Paths.get(HOME_DIR + "data/db");
-    public static final Path APP_DATA_DIR = Paths.get(HOME_DIR + "appdata");
+    public static final Path HOME_APPS_DIR = Paths.get(USER_DIR + "apps");
+    public static final Path DATA_DIR = Paths.get(USER_DIR + "data");
+    public static final Path CONF_DIR = Paths.get(USER_DIR + "conf");
+    public static final Path DB_DIR = Paths.get(USER_DIR + "data/db");
+    public static final Path APP_DATA_DIR = Paths.get(USER_DIR + "appdata");
     public static final Path CONF_FILE = Paths.get(CONF_DIR + "/settings.properties");
 
-    public static final Path LOG_DIR = Paths.get(HOME_DIR + "/log");
+    public static final Path LOG_DIR = Paths.get(HOME_DIR + "system/log");
 
     public static final String DB_URL = "jdbc:h2:" + DB_DIR + "/db;TRACE_LEVEL_FILE=4";
 
-    public static final Path MODULES_DIR = Paths.get(USER_DIR + "modules/");
-    public static final Path APPS_DIR = Paths.get(USER_DIR  + "apps");
-    public static final Path DEFAULT_CONF_FILE = Paths.get(USER_DIR + "conf/settings.properties");
+    public static final Path MODULES_DIR = Paths.get(START_DIR + "modules/");
+    public static final Path APPS_DIR = Paths.get(START_DIR  + "apps");
+    public static final Path DEFAULT_CONF_FILE = Paths.get(START_DIR + "conf/settings.properties");
 
-    private static final String LOCK_DIR = HOME_DIR + "lock/";
+    private static final String LOCK_DIR = USER_DIR + "lock/";
     private static final String LOCK_FILE = LOCK_DIR + "lock.lck";
     private static final String MESSAGE_FILE = LOCK_DIR + "message";
     private final FileLocker fileLocker = new FileLocker(Paths.get(LOCK_FILE), Paths.get(MESSAGE_FILE));
