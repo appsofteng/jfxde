2517eb501f7315c6ab22ea1ce596c3540f44688d
bezvaapp <19367016+bezvaapp@users.noreply.github.com>
2019-09-15 20:06:11 +0200

    Distribution updated.

--- dev.jfxde.parent/dev.jfxde.distribution/src/main/scripts/image/start.bat
+++ dev.jfxde.parent/dev.jfxde.distribution/src/main/scripts/image/start.bat
@@ -4,4 +4,5 @@ rem -Djavafx.animation.framerate
 rem -Djavafx.animation.pulse
 rem -Dprism.vsync=false
 
+cd %~dp0
 start "" runtime\bin\javaw -Djavafx.animation.fullspeed=true -Djava.util.logging.config.file=conf/logging.properties -Dorg.jboss.logging.provider=jdk --add-exports javafx.base/com.sun.javafx.event=org.controlsfx.controls -p lib/mp;modules;apps;"%HOME%/${homeDir}/apps" -cp lib/cp/* -m dev.jfxde.ui
