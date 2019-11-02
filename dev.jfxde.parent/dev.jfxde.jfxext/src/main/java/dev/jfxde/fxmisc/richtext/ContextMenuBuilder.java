6130d547163c4f0cd99290f3e58f44c1ad03d944
bezvaapp <19367016+bezvaapp@users.noreply.github.com>
2019-11-02 13:51:59 +0100

    Fixed resources paths.

--- dev.jfxde.parent/dev.jfxde.jfxext/src/main/java/dev/jfxde/fxmisc/richtext/ContextMenuBuilder.java
+++ dev.jfxde.parent/dev.jfxde.jfxext/src/main/java/dev/jfxde/fxmisc/richtext/ContextMenuBuilder.java
@@ -1,9 +1,12 @@
 package dev.jfxde.fxmisc.richtext;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.fxmisc.richtext.GenericStyledArea;
 
-import dev.jfxde.jfx.util.FXResourceBundle;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.ContextMenu;
@@ -16,6 +19,7 @@ import javafx.scene.input.KeyEvent;
 public class ContextMenuBuilder {
 
     private final GenericStyledArea<?, ?, ?> area;
+    private Map<StringProperty, String> stringProperties = new HashMap<>();
 
     private ContextMenuBuilder(GenericStyledArea<?, ?, ?> codeArea) {
         this.area = codeArea;
@@ -34,7 +38,8 @@ public class ContextMenuBuilder {
     public ContextMenuBuilder copy() {
         MenuItem item = new MenuItem();
         item.setAccelerator(KeyCombination.keyCombination("Shortcut+C"));
-        item.textProperty().bind(FXResourceBundle.getBundle().getStringBinding("copy"));
+        item.setText("Copy");
+        stringProperties.put(item.textProperty(), "copy");
         item.setOnAction(e -> area.copy());
         item.disableProperty().bind(Bindings.createBooleanBinding(() -> area.getSelection().getLength() == 0, area.selectionProperty()));
 
@@ -46,7 +51,8 @@ public class ContextMenuBuilder {
     public ContextMenuBuilder cut() {
         MenuItem item = new MenuItem();
         item.setAccelerator(KeyCombination.keyCombination("Shortcut+X"));
-        item.textProperty().bind(FXResourceBundle.getBundle().getStringBinding("cut"));
+        item.setText("Cut");
+        stringProperties.put(item.textProperty(), "cut");
         item.setOnAction(e -> area.cut());
         item.disableProperty().bind(Bindings.createBooleanBinding(() -> area.getSelection().getLength() == 0, area.selectionProperty()));
 
@@ -59,7 +65,8 @@ public class ContextMenuBuilder {
 
         MenuItem item = new MenuItem();
         item.setAccelerator(KeyCombination.keyCombination("Shortcut+V"));
-        item.textProperty().bind(FXResourceBundle.getBundle().getStringBinding("paste"));
+        item.setText("Paste");
+        stringProperties.put(item.textProperty(), "paste");
         item.setOnAction(e -> area.paste());
 
         area.getContextMenu().setOnShowing(e -> {
@@ -73,7 +80,8 @@ public class ContextMenuBuilder {
     public ContextMenuBuilder selectAll() {
         MenuItem item = new MenuItem();
         item.setAccelerator(KeyCombination.keyCombination("Shortcut+A"));
-        item.textProperty().bind(FXResourceBundle.getBundle().getStringBinding("selectAll"));
+        item.setText("Select All");
+        stringProperties.put(item.textProperty(), "selectAll");
         item.setOnAction(e -> area.selectAll());
         item.disableProperty().bind(Bindings.createBooleanBinding(() -> area.getSelectedText().length() == area.getText().length(),
                 area.selectedTextProperty()));
@@ -85,7 +93,8 @@ public class ContextMenuBuilder {
     public ContextMenuBuilder undo() {
         MenuItem item = new MenuItem();
         item.setAccelerator(KeyCombination.keyCombination("Shortcut+Z"));
-        item.textProperty().bind(FXResourceBundle.getBundle().getStringBinding("undo"));
+        item.setText("Undo");
+        stringProperties.put(item.textProperty(), "undo");
         item.setOnAction(e -> area.undo());
         // item.disableProperty().bind(Bindings.createBooleanBinding(() ->
         // !area.getUndoManager().isUndoAvailable(),
@@ -100,7 +109,8 @@ public class ContextMenuBuilder {
     public ContextMenuBuilder redo() {
         MenuItem item = new MenuItem();
         item.setAccelerator(KeyCombination.keyCombination("Shortcut+Y"));
-        item.textProperty().bind(FXResourceBundle.getBundle().getStringBinding("redo"));
+        item.setText("Redo");
+        stringProperties.put(item.textProperty(), "redo");
         item.setOnAction(e -> area.redo());
         // item.disableProperty().bind(Bindings.createBooleanBinding(() ->
         // !area.getUndoManager().isRedoAvailable(),
@@ -121,7 +131,8 @@ public class ContextMenuBuilder {
 
     public ContextMenuBuilder clear() {
         MenuItem item = new MenuItem();
-        item.textProperty().bind(FXResourceBundle.getBundle().getStringBinding("clear"));
+        item.setText("Clear");
+        stringProperties.put(item.textProperty(), "clear");
         item.setOnAction(e -> area.clear());
         item.disableProperty().bind(Bindings.createBooleanBinding(() -> area.getLength() == 0, area.lengthProperty()));
 
@@ -132,7 +143,8 @@ public class ContextMenuBuilder {
 
     public ContextMenuBuilder clear(EventHandler<ActionEvent> handler) {
         MenuItem item = new MenuItem();
-        item.textProperty().bind(FXResourceBundle.getBundle().getStringBinding("clear"));
+        item.setText("clear");
+        stringProperties.put(item.textProperty(), "clear");
         item.setOnAction(handler);
         item.disableProperty().bind(Bindings.createBooleanBinding(() -> area.getLength() == 0, area.lengthProperty()));
 
